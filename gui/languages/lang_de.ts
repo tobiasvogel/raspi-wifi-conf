1abcd2395c59fc0ee096c7b4b2a4e6644b04e05e
Tobias X. Vogel <tobias.vogel@linux.com>
2020-03-27 16:13:07 +0100

    initial import

---
+++ gui/languages/lang_de.ts
@@ -0,0 +1,356 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>EditHeaders</name>
+    <message>
+        <location filename="../editheaders.cpp" line="10"/>
+        <source>Edit the lines below according to your needs. Be careful not to break the &apos;{{COUNTRYCODE}}&apos; placeholder.</source>
+        <translation>Bearbeiten Sie die nachfolgenden Zeilen entsprechend Ihren Anforderungen. Stellen Sie sicher, den &apos;{{COUNTRYCODE}}&apos;-Platzhalter nicht zu verändern.</translation>
+    </message>
+    <message>
+        <location filename="../editheaders.cpp" line="23"/>
+        <source>A&amp;bort</source>
+        <translation>&amp;Verwerfen</translation>
+    </message>
+    <message>
+        <location filename="../editheaders.cpp" line="25"/>
+        <source>&amp;Apply</source>
+        <translation>Über&amp;nehmen</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Hauptfenster</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="21"/>
+        <source>WiFi</source>
+        <translation>WiFi</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="32"/>
+        <source>SSID</source>
+        <translation>SSID</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="39"/>
+        <source>&amp;Scan</source>
+        <translation>&amp;Scannen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Password</source>
+        <translation>Passwort</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="84"/>
+        <source>Network doesn&apos;t require a password</source>
+        <translation>Dieses Netzwerk benötigt kein Passwort</translation>
+    </message>
+    <message>
+        <source>This is a help Text</source>
+        <translation type="vanished">Dies ist ein Hilfe-Text</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="124"/>
+        <source>Check this Box if your Network does not require any authentication.</source>
+        <translation>Aktivieren Sie dieses Kästchen, wenn Ihr Netzwerk keine Authentifizierung benötigt.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="138"/>
+        <source>This is a hidden Network</source>
+        <translation>Dies ist ein verstecktes Netzwerk</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="178"/>
+        <source>Check this Box if your Network/Router does not broadcast the Name of the WiFi-Network (SSID) publicly.</source>
+        <translation>Aktivieren Sie dieses Kästchen, wenn Ihr Netzwerk/Router den Namen des WiFi-Netzwerks (SSID) nicht aktiv aussendet.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="192"/>
+        <source>Hide WiFi Password in Plain Text from Config File</source>
+        <translation>Das WiFi-Passwort im Klartext nicht in der Konfiguration einschliessen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="235"/>
+        <source>By default the Configuration-File contains both a human readable unencrypted form as well as an encryptet form of your Passphrase. If this Option is checked, the inclusion of the unencrypted human readable form of the Passphrase will be suppressed for an increased Level of Security.</source>
+        <translation>Standardmässig wird in der Konfigurationsdatei sowohl eine für Menschen lesbare, unkodierte Form des Passworts, als auch eine kodierte Form abgelegt. Aktivieren Sie diese Option, um die Hinterlegung des menschenlesbaren, unkodierten Passworts zu verhindern für zusätzliche Sicherheit.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="247"/>
+        <source>Advanced</source>
+        <translation>Erweitert</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="258"/>
+        <source>WiFi Country Code</source>
+        <translation>WiFi Ländercode</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="278"/>
+        <source>URL: https://en.wikipedia.org/wiki/ISO_3166-1
+(opens externally in your default Webbrowser)</source>
+        <translation>URL: https://de.wikipedia.org/wiki/ISO-3166-1-Kodierliste
+(wird extern in Ihrem Standard-Webbrowser geöffnet)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="282"/>
+        <source>&lt;a href=&quot;https://en.wikipedia.org/wiki/ISO_3166-1&quot;&gt;Lookup Online&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;https://de.wikipedia.org/wiki/ISO-3166-1-Kodierliste&quot;&gt;Online Nachschlagen&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="320"/>
+        <source>&gt;AAa</source>
+        <translation>&gt;AAa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="329"/>
+        <source>GB</source>
+        <translation>DE</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="385"/>
+        <source>Different Countries allow WiFi-Networks on different Radio-Frequencies (Channels). Enter the Two-Letter Country Code here of the Place where your WiFi Network is operated.</source>
+        <translation>WiFi-Netzwerke sind in verschiedenen Ländern auf unterschiedlichen Funk-Frequenzen zugelassen. Geben Sie hier den zweistelligen Ländercode des Ortes ein, an dem Sie Ihr Netzwerk betreiben.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="399"/>
+        <source>Don&apos;t include Raspberry Pi-specific Config Lines</source>
+        <translation>Raspberry Pi-spezifische Zeilen nicht einbeziehen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="419"/>
+        <source>Edit</source>
+        <translation>Bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="475"/>
+        <source>In order to make this Tool more versatile, the Raspberry Pi specific Configuration Lines can be excluded from the generated File by enabling this Option.</source>
+        <translation>Um dieses Programm vielseitiger einsetzbar zu machen, lassen sich die für den Raspberry Pi Computer spezifischen Zeilen aus der Konfiguration mithilfe dieser Option unterdrücken.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="489"/>
+        <source>Assign Priority to this Network</source>
+        <translation>Dem Netzwerk eine Priorität zuweisen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="577"/>
+        <source>You can assign different Priorities to different Networks in case more than one Network is available at the same time at any given place. (higher Value eq. higher Priority)</source>
+        <translation>Sie können mittels dieses Werts verschiedenen Netzwerken verschiedene Prioritätsstufen zuweisen, für den Fall, dass sich mehrere Netzwerke gleichzeitig in Reichweite befinden. (höherer Wert entspricht höherer Priorität)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="591"/>
+        <source>Provide Identification String</source>
+        <translation>Netzwerk Bezeichnung (Identifikation)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="667"/>
+        <source>Any Identifier entered here will make it easier to recognise Network Configurations in 3rd party Apps. (ex. &quot;Home&quot;)</source>
+        <translation>Geben Sie hier eine Bezeichnung ein, um in externen, unabhängigen Apps eine bestimmte Netzwerk-Konfiguration leichter wiederzufinden. (Bsp. &quot;Zuhause&quot;)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="681"/>
+        <source>Create &apos;ssh&apos;-file for Headless Access</source>
+        <translation>&apos;ssh&apos;-Datei für Kommandozeilen-Zugriff erstellen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="721"/>
+        <source>A Raspberry Pi Computer can be accessed easily using the Secure Shell Protocol if an empty File called &apos;ssh&apos; is placed in the &apos;boot&apos;-Directory prior to the first System Startup. Use this Option to conveniently also create a &apos;ssh&apos;-File alongside with the Network Configuration.</source>
+        <translation>Es ist möglich, auf einen Raspberry Pi direkt über das Secure Shell Protokoll zuzugreifen, wenn eine leere Datei mit Namen &apos;ssh&apos; vor dem ersten Systemstart im &apos;boot&apos;-Verzeichnis plaziert wird. Mittels dieser Option wird zusätzlich zur Netzwek-Konfiguration eine solche &apos;ssh&apos;-Datei erzeugt.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="756"/>
+        <source>Output</source>
+        <translation>Zielpfad</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="767"/>
+        <source>Output Directory</source>
+        <translation>Ziel-Verzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="774"/>
+        <source>&amp;Browse...</source>
+        <translation>&amp;Durchsuchen...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="788"/>
+        <source>Append Network if Config File exists</source>
+        <translation>Netzwerk an vorhandene Konfiguration (falls vorhanden) anhängen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="828"/>
+        <source>If you would like to have multiple Networks Configured in a single Configuration check this Box after an initial Configuration File Generation and any further Network Configuration will simply be appended to the existing Configuration.</source>
+        <translation>Für den Fall, dass Sie mehrere Netzwerk-Konfigurationen auf einmal in einer Konfigurationsdatei hinterlegen möchten, aktivieren Sie diese Option nach der erstmaligen Erstellung einer Konfiguration, um direkt weitere Netzwerk-Konfigurationen daran anzufügen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="878"/>
+        <source>&amp;About</source>
+        <translation>Übe&amp;r</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="898"/>
+        <source>&amp;Generate</source>
+        <translation>&amp;Erstellen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="921"/>
+        <source>&amp;Quit</source>
+        <translation>Bee&amp;nden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="21"/>
+        <source>WPA Configuration</source>
+        <translation>WPA Konfiguration</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="123"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <source>Open Directory</source>
+        <translation>Ordner öffnen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>&apos;ssh&apos;-File already exists in the Directory specified.</source>
+        <translation>Eine &apos;ssh&apos;-Datei existiert bereits im angegebenen Verzeichnis.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="194"/>
+        <source>Error creating the &apos;ssh&apos;-File. Check your File-Permissions.</source>
+        <translation>Fehler beim Erstellen der &apos;ssh&apos;-Datei. Überprüfen Sie Ihre Dateizugriffsrechte.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="252"/>
+        <source>Config File already exists. Check the corresponding Option if you want to Append another Network Configuration to it.</source>
+        <translation>Die Konfigurationsdatei existiert bereits. Wählen Sie die entsprechende Option, wenn Sie ein weiteres Netzwerk an eine bestehende Konfiguration anfügen möchten.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="258"/>
+        <source>Error opening the Config File. Check your File-Permissions.</source>
+        <translation>Fehler beim Öffnen der Konfigurationsdatei. Überprüfen Sie Ihre Datei-Zugriffsrechte.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="262"/>
+        <source>You can still Manually Copy the Generated Output to a File called &quot; wpa_supplicant.conf &quot;.
+Would you like me to Display the generated File-Contents in order for you to Copy it?</source>
+        <translation>Sie können die generierte Konfiguration manuell kopieren und in eine leere Datei unter dem Namen &quot; wpa_supplicant.conf &quot; einfügen.
+Möchten Sie, dass ich die generierten Konfigurationsdaten jetzt anzeige, damit Sie sie kopieren können?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="280"/>
+        <source>File(s) generated successfully!</source>
+        <translation>Datei(en) erfolgreich erstellt!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="294"/>
+        <source>Copy the Lines below (&quot; wpa_supplicant.conf &quot;):</source>
+        <translation>Kopieren Sie die nachfolgenden Zeilen (&quot; wpa_supplicant.conf &quot;):</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="304"/>
+        <source>Copy to Clipboard</source>
+        <translation>In die Zwischenablage kopieren</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="307"/>
+        <source>Close</source>
+        <translation>Schliessen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="321"/>
+        <source>Success</source>
+        <translation>Erfolg</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="321"/>
+        <source>Successfully copied data to Clipboard.</source>
+        <translation>Daten erfolgreich in die Zwischenablage kopiert.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="331"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="332"/>
+        <source>This Program is released under the GPLv2 License.</source>
+        <translation>Dieses Programm ist unter der GPLv2 Lizenz freigegeben.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="334"/>
+        <source>The &quot;wpa_supplicant&quot; utility is released under the BSD License (see %1)</source>
+        <translation>Das &quot;wpa_supplicant&quot;-Werkzeug ist unter der BSD Lizenz freigegeben (siehe %1)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="336"/>
+        <source>%1 2020 by %2</source>
+        <translation>%1 2020 %2</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="338"/>
+        <source>Please report Bugs at %1</source>
+        <translation>Bitte melden Sie Fehler auf %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="340"/>
+        <source>Contributions and Translations welcome! Please submit Pull Requests via GitHub at %1</source>
+        <translation>Ergänzungen und Übersetzungen sind jederzeit willkommen! Bitte reichen Sie einen entsprechenden Request über GitHub ein unter %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="351"/>
+        <source>No SSID ha-s been specified.</source>
+        <translation>Keine SSID eingegeben.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="358"/>
+        <source>A WiFi-Passphrase must contain at least 8 characters.</source>
+        <translation>Ein WiFi-Passwort muss aus mindestens 8 Zeichen bestehen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="363"/>
+        <source>A WiFi-Passphrase cannot exceed 63 characters in length.</source>
+        <translation>Ein WiFi-Passwort darf höchstens 63 Zeichen umfassen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="368"/>
+        <source>No Output directory specified.</source>
+        <translation>Kein Ziel-Verzeichnis angegeben.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="373"/>
+        <source>The Passphrase you entered contains invalid characters.</source>
+        <translation>Das eingegebene Passwort enthält ungültige Zeichen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="387"/>
+        <source>The Output Directory specified does not exist.</source>
+        <translation>Das angegebene Ziel-Verzeichnis existiert nicht.</translation>
+    </message>
+</context>
+<context>
+    <name>WifiList</name>
+    <message>
+        <location filename="../wifilist.cpp" line="12"/>
+        <source>Select any of the Networks to be used:</source>
+        <translation>Wählen Sie eines der Netzwerke unten aus, um es zu verwenden:</translation>
+    </message>
+    <message>
+        <location filename="../wifilist.cpp" line="31"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../wifilist.cpp" line="32"/>
+        <source>&amp;Use</source>
+        <translation>&amp;Verwenden</translation>
+    </message>
+</context>
+</TS>
