506de0bd0fca515fd7a584dd9f8765223e0481d5
Tobias X. Vogel <tobias.vogel@linux.com>
2020-03-27 16:21:52 +0100

    reflected latest changes in automated translations generation in source

--- gui/main.cpp
+++ gui/main.cpp
@@ -13,10 +13,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QTranslator translator;
-    QString m_langPath = QApplication::applicationDirPath();
-    m_langPath.append(QDir::separator());
-    m_langPath.append("languages");
-    m_langPath.append(QDir::separator());
+    QString m_langPath = ":/i18n/";
     m_langPath.append("lang_" + QLocale().name().split('_').at(0) + ".qm");
     translator.load(m_langPath);
     a.installTranslator(&translator);
